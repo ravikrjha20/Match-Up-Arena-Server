dbcc1bc9e4b68ee3ca0df9ba42f21b5d66e9ce00
Ravi Kumar Jha <jhakrravi20@gmail.com>
2025-08-24 16:43:48 +0530

    secure cookie

--- utils/jwt.js
+++ utils/jwt.js
@@ -20,13 +20,13 @@ const attachCookiesToResponse = ({ res, user, refreshToken }) => {
 
   const oneDay = 1000 * 60 * 60 * 24;
   const longerExp = 1000 * 60 * 60 * 24 * 30;
-
+  const isProduction = process.env.NODE_ENV === "production";
   res.cookie("accessToken", accessTokenJWT, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     signed: true,
     expires: new Date(Date.now() + oneDay),
-    sameSite: "none",
+    sameSite: isProduction ? "none" : "lax",
   });
 
   res.cookie("refreshToken", refreshTokenJWT, {
@@ -34,7 +34,7 @@ const attachCookiesToResponse = ({ res, user, refreshToken }) => {
     secure: process.env.NODE_ENV === "production",
     signed: true,
     expires: new Date(Date.now() + longerExp),
-    sameSite: "none",
+    sameSite: isProduction ? "none" : "lax",
   });
 };
 
